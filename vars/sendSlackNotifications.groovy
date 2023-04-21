fe12a8b726d200b3cb094401ec56c5a28ba970cc
Jerold <111006157+Jean2425@users.noreply.github.com>
2023-04-21 23:54:40 +0530

    Update sendSlackNotifications.groovy

--- vars/sendSlackNotifications.groovy
+++ vars/sendSlackNotifications.groovy
@@ -2,7 +2,7 @@ def call(String buildStatus = 'STARTED') {
   // build status of null means successful
   //This is the condition which we are checking weather buildStatus is SUCCESSFULL or not.
  //This line updated to show the Eclipse with GitHub demo
-  buildStatus =  buildStatus ?: 'SUCCESS':"FAILURE"
+  buildStatus =  buildStatus ?:'SUCCESS'
   
   // Default values
   def colorName = 'RED'
@@ -11,16 +11,17 @@ def call(String buildStatus = 'STARTED') {
   def summary = "${subject} (${env.BUILD_URL})"
 
   // Override default values based on build status
-  if (buildStatus == 'STARTED') {
-    color = 'YELLOW'
-    colorCode = '#FFFF00'
+ if (buildStatus == 'STARTED') {
+    colorName = 'ORANGE'
+    colorCode = '#FFA500'
   } else if (buildStatus == 'SUCCESS') {
-    color = 'GREEN'
+    colorName = 'GREEN'
     colorCode = '#00FF00'
   } else {
-    color = 'RED'
+    colorName = 'RED'
     colorCode = '#FF0000'
   }
+
 
   // Calling the slackSend function to Send notifications.
   slackSend (color: colorCode, message: summary, channel: "#declarative-way")
